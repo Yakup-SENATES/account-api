fb0c5dabf2b12bfd5b170dd3830b39558fe7647c
Yakup SENATES <76456346+Yakup-SENATES@users.noreply.github.com>
2022-07-29 01:00:04 +0300

    1.1

---
+++ account-api/src/test/java/com/example/demo/IntegrationTestSupport.java
@@ -0,0 +1,5 @@
+package com.example.demo;
+
+public abstract class IntegrationTestSupport  extends TestSupport {
+
+}
--- account-api/src/test/java/com/example/demo/controller/AccountControllerTest.java
+++ account-api/src/test/java/com/example/demo/controller/AccountControllerTest.java
@@ -1,28 +1,39 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.CreateAccountRequest;
 import com.example.demo.dto.converter.AccountDtoConverter;
+import com.example.demo.model.Customer;
 import com.example.demo.repository.AccountRepository;
+import com.example.demo.repository.CustomerRepository;
 import com.example.demo.service.AccountService;
 import com.example.demo.service.CustomerService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.time.Clock;
 import java.util.UUID;
 import java.util.function.Supplier;
 
+import static org.hamcrest.Matchers.*;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
@@ -59,6 +70,9 @@ class AccountControllerTest {
 
     private static final UUID uuid = UUID.randomUUID();
 
+    @Autowired
+    private CustomerRepository  customerRepository;
+
     @BeforeEach
     public void setUp() {
 
@@ -67,5 +81,22 @@ class AccountControllerTest {
 
     }
 
+    @Test
+    public void testCreateAccount_whenCustomerIdExist_shouldCreateAccountAndReturnAccountDto() throws Exception{
+        Customer customer = customerRepository.save(new Customer("Jacop","Handler"));
+        CreateAccountRequest request =  new CreateAccountRequest(customer.getId(),new BigDecimal(100));
+
+        this.mockMvc.perform(post("/v1/accounts")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writer().withDefaultPrettyPrinter()
+                        .writeValueAsString(request)))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", notNullValue()))
+                .andExpect(jsonPath("$.balance", is(100)))
+                .andExpect(jsonPath("$.customer",is(mapper.writer().withDefaultPrettyPrinter().writeValueAsString(customer))))
+                .andExpect(jsonPath("$.transaction").isArray());
+
+    }
 
 }
